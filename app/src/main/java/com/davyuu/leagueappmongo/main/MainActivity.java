00125c0b6bf9c43fda48609b66e24b07bab78dbf
David Yu <davidyu_2005@hotmail.com>
2017-01-22 14:39:28 -0500

    add view pager

--- app/src/main/java/com/davyuu/leagueappmongo/main/MainActivity.java
+++ app/src/main/java/com/davyuu/leagueappmongo/main/MainActivity.java
@@ -1,23 +1,46 @@
 package com.davyuu.leagueappmongo.main;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.davyuu.leagueappmongo.R;
 
+import static com.davyuu.leagueappmongo.main.MainViewPagerAdapter.PAGE_ITEM_FRAGMENT;
+
 public class MainActivity extends AppCompatActivity {
 
+    ViewPager viewPager;
     MainViewPagerAdapter mainViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_layout);
+        viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+
 
         setupMainViewPager();
     }
 
     private void setupMainViewPager() {
         mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(mainViewPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // TODO: 2017-01-22  
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
--- app/src/main/java/com/davyuu/leagueappmongo/main/MainViewPagerAdapter.java
+++ app/src/main/java/com/davyuu/leagueappmongo/main/MainViewPagerAdapter.java
@@ -8,7 +8,9 @@ import com.davyuu.leagueappmongo.item.ItemFragment;
 import com.davyuu.leagueappmongo.second.SecondFragment;
 
 public class MainViewPagerAdapter extends FragmentStatePagerAdapter {
-    public static final int NUM_PAGES = 2;
+    public static final int PAGE_ITEM_FRAGMENT = 0;
+    public static final int PAGE_SECOND_FRRAGMENT = 1;
+    private static final int NUM_PAGES = 2;
     public Fragment[] fragmentList = new Fragment[NUM_PAGES];
 
     public MainViewPagerAdapter(FragmentManager fm) {
@@ -17,20 +19,21 @@ public class MainViewPagerAdapter extends FragmentStatePagerAdapter {
 
     @Override
     public Fragment getItem(int position) {
-        if (position == 0) {
-            if(fragmentList[position] == null){
-                ItemFragment itemFragment = ItemFragment.newInstance();
-                fragmentList[position] = itemFragment;
-            }
-            return fragmentList[position];
-        } else if (position == 1) {
-            if(fragmentList[position] == null){
-                SecondFragment secondFragment = SecondFragment.newInstance();
-                fragmentList[position] = secondFragment;
-            }
-            return fragmentList[position];
-        } else {
-            return ItemFragment.newInstance();
+        switch (position) {
+            case PAGE_ITEM_FRAGMENT:
+                if (fragmentList[position] == null) {
+                    ItemFragment itemFragment = ItemFragment.newInstance();
+                    fragmentList[position] = itemFragment;
+                }
+                return fragmentList[position];
+            case PAGE_SECOND_FRRAGMENT:
+                if (fragmentList[position] == null) {
+                    SecondFragment secondFragment = SecondFragment.newInstance();
+                    fragmentList[position] = secondFragment;
+                }
+                return fragmentList[position];
+            default:
+                return ItemFragment.newInstance();
         }
     }
 
