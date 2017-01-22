70b71236e133d8d307840c8eec5438be99b90826
davyuu <davidyu_2005@hotmail.com>
2017-01-22 13:36:04 -0500

    add main view pager

--- app/src/main/java/com/davyuu/leagueappmongo/main/MainActivity.java
+++ app/src/main/java/com/davyuu/leagueappmongo/main/MainActivity.java
@@ -7,9 +7,17 @@ import com.davyuu.leagueappmongo.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    MainViewPagerAdapter mainViewPagerAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupMainViewPager();
+    }
+
+    private void setupMainViewPager() {
+        mainViewPagerAdapter = new MainViewPagerAdapter(getSupportFragmentManager());
     }
 }
